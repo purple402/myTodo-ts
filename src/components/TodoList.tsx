bc196e30f734a8bf1e35a0ba33f9f25d7a293b01
Joy Lee <purple402@naver.com>
2022-08-09 21:19:44 +0900

    Create TodoItem component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray;
 `;
 
 function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem id={1} done={false} text="과제하기" />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
