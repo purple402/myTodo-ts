74f632a45af86045e43b6fb6ca4efb28ecb93f36
Joy Lee <purple402@naver.com>
2022-08-11 17:43:53 +0900

    Create custom hooks in TodoContext

--- src/TodoContext.tsx
+++ src/TodoContext.tsx
@@ -1,4 +1,11 @@
-import React, { useReducer, Dispatch, createContext } from 'react';
+import React, {
+  useReducer,
+  Dispatch,
+  createContext,
+  useContext,
+  useRef,
+  RefObject,
+} from 'react';
 
 type Todo = {
   id: number;
@@ -55,26 +62,37 @@ function todoReducer(state: State, action: Action): State {
 
 const TodoStateContext = createContext<State | null>(null);
 const TodoDispatchContext = createContext<DispatchType | null>(null);
+const TodoNextIdContext = createContext<RefObject<number> | null>(null);
 
 export function TodoProvider({ children }: { children: React.ReactNode }) {
   const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  const nextId = useRef<number>(5);
+
   return (
     <TodoStateContext.Provider value={state}>
       <TodoDispatchContext.Provider value={dispatch}>
-        {children}
+        <TodoNextIdContext.Provider value={nextId}>
+          {children}
+        </TodoNextIdContext.Provider>
       </TodoDispatchContext.Provider>
     </TodoStateContext.Provider>
   );
 }
 
-// export function useTodoState() {
-//   const state = useContext(TodoStateContext);
-//   if (!state) throw new Error('cannot find TodoProvider');
-//   return state;
-// }
+export function useTodoState() {
+  const state = useContext(TodoStateContext);
+  if (!state) throw new Error('cannot find TodoProvider');
+  return state;
+}
 
-// export function useTodoDispatch() {
-//   const dispatch = useContext(TodoDispatchContext);
-//   if (!dispatch) throw new Error('cannot find TodoProvider');
-//   return dispatch;
-// }
+export function useTodoDispatch() {
+  const dispatch = useContext(TodoDispatchContext);
+  if (!dispatch) throw new Error('cannot find TodoProvider');
+  return dispatch;
+}
+
+export function useTodoNextId() {
+  const nextId = useContext(TodoNextIdContext);
+  if (!nextId) throw new Error('cannot find TodoProvider');
+  return nextId;
+}
